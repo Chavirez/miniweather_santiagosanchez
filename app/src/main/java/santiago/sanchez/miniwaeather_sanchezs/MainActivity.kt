f1fb3adf26501b9aae9ed4e5393075ee34157b54
Chavirez <santiagosanchezch@gmail.com>
2026-02-03 13:22:35 -0700

    miniweather

---
+++ app/src/main/java/santiago/sanchez/miniwaeather_sanchezs/MainActivity.kt
@@ -0,0 +1,34 @@
+package santiago.sanchez.miniwaeather_sanchezs
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvCity: TextView
+    private lateinit var ivWeather: ImageView
+    private lateinit var tvTemperature: TextView
+    private lateinit var tvGreeting: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        tvCity = findViewById(R.id.tvCity)
+        ivWeather = findViewById(R.id.ivWeather)
+        tvTemperature = findViewById(R.id.tvTemperature)
+        tvGreeting = findViewById(R.id.tvGreeting)
+
+    }
+}
